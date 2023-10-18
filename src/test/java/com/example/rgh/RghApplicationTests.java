94cd48b18fd8c02eff9cd6099a1d80eea001d7a8
rogal <gorimooze@gmail.com>
2023-10-18 14:27:09 +0300

    Git init

---
+++ src/test/java/com/example/rgh/RghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rgh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RghApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
